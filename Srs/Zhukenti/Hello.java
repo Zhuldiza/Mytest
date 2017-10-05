3cf46db07c8a875ac1068f6f7b96cf3bd11c54f1
Zhuka <peppi_23@mail.ru>
2017-10-05 23:57:11 +0600

    experiment1  bytaginda  Hello.java   jane MyMath.java  failnda tagida ozgerister jasaldi

--- Srs/Zhukenti/Hello.java
+++ Srs/Zhukenti/Hello.java
@@ -2,9 +2,9 @@ package Zhukenti;
 
 public class Hello {
     public static void main(String[] args){
-        System.out.println("Hello Zhuka!");
-        System.out.println("Byl zertxanalik jumis!");
-        System.out.println("GitHub-pen jumis jasap uirenu!");
+        int a= MyMath.kvadrat(a=5);
+        int b= MyMath.kvadrat(b=5);
+
 
         int a =MyMath.kvadrat(a=5);
         int b= MyMath.kub(a=5);
@@ -13,5 +13,11 @@ public class Hello {
         System.out.println("b= " +b);
 
         System.out.println(MyMath.max3(a=8 ,b=9 , c=12));
+
+        System.out.println("a= " +a);
+        System.out.println("b= " +b);
+
+        
+
     }
 }
