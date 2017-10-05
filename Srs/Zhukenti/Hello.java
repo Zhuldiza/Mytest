b4b3fa5c5eefdf33e641083fdf73f492fedb1108
Zhuka <peppi_23@mail.ru>
2017-10-05 23:00:28 +0600

    Екінші коммит

--- Srs/Zhukenti/Hello.java
+++ Srs/Zhukenti/Hello.java
@@ -3,5 +3,7 @@ package Zhukenti;
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello Zhuka!");
+        System.out.println("Byl zertxanalik jumis!");
+        System.out.println("GitHub-pen jumis jasap uirenu!");
     }
 }
